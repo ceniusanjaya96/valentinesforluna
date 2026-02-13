2f9b2d419c5d03f42ace964b387dc5bb8ac24edb
ceniusanjaya96 <ceniusanjaya@gmail.com>
2026-02-13 14:12:37 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,88 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+const sticker = document.getElementById("sticker");
+const letterOverlay = document.getElementById("letterOverlay");
+const closeLetter = document.getElementById("closeLetter");
+const music = document.getElementById("bgMusic");
+
+// ================= YES CLICK =================
+yesBtn.addEventListener("click", function() {
+
+    // ganti sticker happy
+    sticker.src = "images/happy.png";
+    message.innerHTML = "I knew it 😭💖";
+
+    // background berubah
+    document.body.style.background =
+    "linear-gradient(to right, #ff4d88, #ff99cc)";
+
+    // play lagu
+    music.volume = 0;
+    music.play();
+
+    // fade in volume
+    let vol = 0;
+    const fade = setInterval(() => {
+        if(vol < 0.5){
+            vol += 0.02;
+            music.volume = vol;
+        } else {
+            clearInterval(fade);
+        }
+    }, 200);
+
+    // munculin surat
+    setTimeout(() => {
+        letterOverlay.style.display = "flex";
+    }, 500);
+});
+
+
+// ================= NO BUTTON AUTO KABUR =================
+document.addEventListener("mousemove", function(e) {
+
+    const rect = noBtn.getBoundingClientRect();
+
+    const distance = 100;
+
+    const btnX = rect.left + rect.width / 2;
+    const btnY = rect.top + rect.height / 2;
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const dx = mouseX - btnX;
+    const dy = mouseY - btnY;
+
+    const dist = Math.sqrt(dx * dx + dy * dy);
+
+    if (dist < distance) {
+
+        const newX = Math.random() * (window.innerWidth - 100);
+        const newY = Math.random() * (window.innerHeight - 50);
+
+        noBtn.style.position = "absolute";
+        noBtn.style.left = newX + "px";
+        noBtn.style.top = newY + "px";
+
+        const images = [
+            "images/cry1.png",
+            "images/cry2.png",
+            "images/sad.png"
+        ];
+
+        sticker.src = images[Math.floor(Math.random() * images.length)];
+        message.innerHTML = "Pliss jangan pilih NO 😭";
+
+        // YES makin gede tiap dia gagal ngejar NO 😈
+        yesBtn.style.transform += " scale(1.05)";
+    }
+});
+
+
+// ================= CLOSE LETTER =================
+closeLetter.addEventListener("click", function() {
+    letterOverlay.style.display = "none";
+    music.pause();
+});
